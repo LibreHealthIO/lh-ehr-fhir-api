f7b8bc312bc928a4ac1a3c7ee88dd31bce4c59a5
Ken Chapple <ken.chapple@gmail.com>
2016-07-22 15:03:20 -0400

    added core bindings to fhir service provider

--- src/Utilities/Providers/FHIRServiceProvider.php
+++ src/Utilities/Providers/FHIRServiceProvider.php
@@ -14,6 +14,23 @@ class FHIRServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // Bind the OpenEMR implementations to the interface
+        $this->app->bind('LibreEHR\Core\Contracts\PatientRepositoryInterface', 'LibreEHR\Core\Emr\Repositories\PatientRepository');
+        $this->app->bind('LibreEHR\Core\Contracts\PatientFinderInterface', 'LibreEHR\Core\Emr\Finders\PatientFinder');
+        $this->app->bind('LibreEHR\Core\Contracts\PatientInterface', 'LibreEHR\Core\Emr\Eloquent\PatientData', function( $app ) {
+            return new \LibreEHR\Core\Emr\Eloquent\PatientData();
+        });
+
+        $this->app->bind('LibreEHR\Core\Contracts\DocumentRepositoryInterface', 'LibreEHR\Core\Emr\Repositories\DocumentRepository');
+        $this->app->bind('LibreEHR\Core\Contracts\DocumentInterface', 'LibreEHR\Core\Emr\Eloquent\Document', function( $app ) {
+            return new \LibreEHR\Core\Emr\Eloquent\Document();
+        });
+
+        $this->app->bind('LibreEHR\Core\Contracts\AuditEventRepositoryInterface', 'LibreEHR\Core\Emr\Repositories\AuditEventRepository');
+        $this->app->bind('LibreEHR\Core\Contracts\AuditEventInterface', 'LibreEHR\Core\Emr\Eloquent\AuditEvent', function( $app ) {
+            return new \LibreEHR\Core\Emr\Eloquent\AuditEvent();
+        });
+
         $this->app->bind('LibreEHR\Core\Contracts\AuditEventAdapterInterface', 'LibreEHR\FHIR\Adapters\FHIRAuditEventAdapter');
         $this->app->bind('LibreEHR\Core\Contracts\PatientAdapterInterface', 'LibreEHR\FHIR\Adapters\FHIRPatientAdapter');
         $this->app->bind('LibreEHR\Core\Contracts\BundleAdapterInterface', 'LibreEHR\FHIR\Adapters\FHIRBundleAdapter');
