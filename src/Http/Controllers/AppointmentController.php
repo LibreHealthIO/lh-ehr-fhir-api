211ecff598244237318a316835e7161625e7a539
Leo24 <leo2410@yandex.ru>
2016-08-04 17:59:00 +0300

    Add methods for Appointment Slot resource

--- src/Http/Controllers/AppointmentController.php
+++ src/Http/Controllers/AppointmentController.php
@@ -2,18 +2,48 @@
 
 namespace LibreEHR\FHIR\Http\Controllers;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use LibreEHR\Core\Contracts\PatientAdapterInterface;
-use LibreEHR\Core\Contracts\PatientFinderInterface;
-use LibreEHR\Core\Contracts\PatientRepositoryInterface;
+use LibreEHR\FHIR\Adapters\FHIRAppointmentAdapter;
+use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\DocBlock\Tags\Return_;
 
-class PatientController extends AbstractController
+class AppointmentController extends AbstractController
 {
-    public function __construct( PatientAdapterInterface $patientAdapter )
+    public function __construct()
     {
-        $this->adapter = $patientAdapter;
+        //$this->adapter = new FHIRAppointmentAdapter;
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function getSchedule()
+    {
+        $results = DB::select('select * from openemr_postcalendar_events');
+        $json = json_encode($results);
+
+        return $json;
+    }
+
+    public function addSlot( Request $request )
+    {
+        $start = $request->input('start');
+        $end = $request->input('end');
+        $pid = $request->input('pid');
+
+        $result = DB::insert('insert into openemr_postcalendar_events (pc_pid, pc_startTime, pc_endTime) values (?, ?, ?)', [$pid, $start, $end]);
+
+        return json_encode($result);
+    }
+
+    public function checkStatus(  Request $request )
+    {
+        $pc_eid = $request->input('pc_eid');
+        $result = DB::select('select pc_apptstatus from openemr_postcalendar_events  where pc_eid = ?', [$pc_eid]);
+
+        return json_encode($result);
     }
 
 }
--- src/Http/routes.php
+++ src/Http/routes.php
@@ -5,7 +5,10 @@ Route::group(['prefix' => 'fhir'], function () {
     // See resource routes in Laravel Docs
     Route::resource('Patient', '\LibreEHR\FHIR\Http\Controllers\PatientController');
     Route::resource('Patients', '\LibreEHR\FHIR\Http\Controllers\PatientController');
-    Route::resource('Appointment', '\LibreEHR\FHIR\Http\Controllers\AppointmentController');
+
+    Route::resource('get_schedule', '\LibreEHR\FHIR\Http\Controllers\AppointmentController@getSchedule');
+    Route::resource('add_slot', '\LibreEHR\FHIR\Http\Controllers\AppointmentController@addSlot');
+    Route::resource('check_status', '\LibreEHR\FHIR\Http\Controllers\AppointmentController@checkStatus');
 
     Route::resource('conformance', '\LibreEHR\FHIR\Http\Controllers\ConformanceController');
     Route::resource('metadata', '\LibreEHR\FHIR\Http\Controllers\ConformanceController');
