24470e98e86e2828dc884ab250916ccd85fda40b
Leo24 <leo2410@yandex.ru>
2016-08-22 16:57:16 +0300

    Added route for creating Patient

--- src/Http/Controllers/PatientController.php
+++ src/Http/Controllers/PatientController.php
@@ -4,9 +4,21 @@ namespace LibreEHR\FHIR\Http\Controllers;
 
 use App\Http\Requests;
 use LibreEHR\Core\Contracts\BaseAdapterInterface;
+use Illuminate\Http\Request;
 
 class PatientController extends AbstractController
 {
 
+    protected $patientAdapter = null;
+
+    public function __construct( BaseAdapterInterface $patientAdapter )
+    {
+        $this->patientAdapter = $patientAdapter;
+    }
+
+    public function post(Request $request)
+    {
+        return $this->patientAdapter->store($request);
+    }
 }
 
