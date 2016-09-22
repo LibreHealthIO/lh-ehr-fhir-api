b19afcbde7b5597103054f6418888aa94b14ef0f
Leo24 <leo2410@yandex.ru>
2016-09-22 17:40:31 +0300

    Add functionality for ValueSet Resource/ Try to add mapping for providers and pharmacies

--- src/Http/Controllers/ValuesetController.php
+++ src/Http/Controllers/ValuesetController.php
@@ -2,8 +2,62 @@
 
 namespace LibreEHR\FHIR\Http\Controllers;
 
+use App\Http\Requests;
+use LibreEHR\Core\Contracts\BaseAdapterInterface;
+use Illuminate\Http\Request;
 
 class ValuesetController extends AbstractController
 {
 
+//    protected $valuesetAdapter = null;
+    protected $providerAdapter = null;
+    protected $pharmacyAdapter = null;
+
+    public function __construct(BaseAdapterInterface $pharmacyAdapter, BaseAdapterInterface $providerAdapter)
+    {
+        $this->providerAdapter = $providerAdapter;
+        $this->pharmacyAdapter = $pharmacyAdapter;
+//        $this->middleware('auth:api');
+    }
+
+    public function index(Request $request = null)
+    {
+        $adapter = $this->setAdapter($request);
+        return $adapter->collectionToOutput();
+    }
+
+    public function show($id)
+    {
+//        $this->setAdapter($request);
+        return $this->valuesetAdapter->show($id);
+    }
+
+    public function post(Request $request)
+    {
+//        $this->setAdapter($request);
+        return $this->valuesetAdapter->store($request);
+    }
+
+
+    public function update(Request $request)
+    {
+//        $this->setAdapter($request);
+        return $this->valuesetAdapter->update($request);
+    }
+
+    public function destroy($id)
+    {
+//        $this->setAdapter($request);
+        return $this->valuesetAdapter->remove($id);
+    }
+
+    private function setAdapter($request)
+    {
+        if ($request->header('type') == 'provider') {
+            $this->valuesetAdapter = $this->providerAdapter;
+        }
+        if ($request->header('type')== 'pharmacy') {
+            $this->valuesetAdapter = $this->pharmacyAdapter;
+        }
+    }
 }
