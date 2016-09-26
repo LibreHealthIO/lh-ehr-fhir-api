23dc5a388b03c56d154f7fe1033cc1bad48086f2
Leonid <leo2410@yandex.ru>
2016-09-26 16:31:23 +0300

    Add response for login route on Unauthorised request (#41)

--- src/Http/authRoutes.php
+++ src/Http/authRoutes.php
@@ -4,6 +4,6 @@ Route::post('/signup', 'Auth\RegisterController@register');
 Route::post('/signin', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
 Route::post('/recover-password', 'Auth\ForgotPasswordController@sendResetLinkEmail');
 Route::post('/recover-username', 'Auth\ForgotPasswordController@recoverUsernameByPhone');
-
-
-
+Route::get('/login', function () {
+    return json_encode([ 'error' => 'Unauthorised.', 'code' => 401], true);
+});
