dca13aa21d51e4789e083f0ea85207b3e93aea1a
Ken Chapple <ken.chapple@gmail.com>
2016-10-12 14:55:22 -0400

    removed uneccesary adapter in subclass causing exception

--- src/Http/Controllers/SlotController.php
+++ src/Http/Controllers/SlotController.php
@@ -6,16 +6,9 @@ use Illuminate\Http\Request;
 
 class SlotController extends AbstractController
 {
-    protected $slotAdapter = null;
-
-    public function __construct( BaseAdapterInterface $slotAdapter )
-    {
-        $this->slotAdapter = $slotAdapter;
-    }
-
     public function index(Request $request = null)
     {
         $this->init();
-        return $this->slotAdapter->getSlots($request);
+        return $this->adapter->getSlots($request);
     }
 }
