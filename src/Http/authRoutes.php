7aee7c9cbc64f5ed2ec2937cd408f5c8ab66e37c
Ken Chapple <ken.chapple@gmail.com>
2016-10-19 18:08:43 -0400

    add password routes to auth routes

--- src/Http/authRoutes.php
+++ src/Http/authRoutes.php
@@ -12,6 +12,12 @@ Route::post('/recover-password', 'Auth\ForgotPasswordController@sendResetLinkEma
 // Recover username using phone
 Route::post('/recover-username', 'Auth\ForgotPasswordController@recoverUsernameByPhone');
 
+// Password Reset Routes...
+Route::get('password/reset', 'LibreEHR\FHIR\Http\Controllers\Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'LibreEHR\FHIR\Http\Controllers\Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'LibreEHR\FHIR\Http\Controllers\Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'LibreEHR\FHIR\Http\Controllers\Auth\ResetPasswordController@reset');
+
 Route::get('/login', function () {
     return json_encode([ 'error' => 'Unauthorised.', 'code' => 401], true);
 });
