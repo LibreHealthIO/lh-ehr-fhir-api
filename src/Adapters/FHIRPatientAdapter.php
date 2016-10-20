aa903d5894e8bfce56a2d426618d7f99d48d327a
Leo24 <leo2410@yandex.ru>
2016-10-20 14:05:24 +0300

    Remove extra set of patient id. Сauses updating wrong patient.

--- src/Adapters/FHIRPatientAdapter.php
+++ src/Adapters/FHIRPatientAdapter.php
@@ -136,7 +136,7 @@ class FHIRPatientAdapter extends AbstractFHIRAdapter implements BaseAdapterInter
         if ($user->connection == $connection &&
             $user->ehr_pid
         ) {
-            $patientInterface->setPid( $user->ehr_pid );
+//            $patientInterface->setPid( $user->ehr_pid );
             // We already have a patient link in the EHR database using this connection
             $patientInterface = $this->repository->update($patientInterface);
         } else {
