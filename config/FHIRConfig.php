6d797d78a97aeedb36ab92779a746054f30271b1
Ken Chapple <ken.chapple@gmail.com>
2017-01-05 10:40:21 -0500

    adding oxygen sms relay connector

--- config/FHIRConfig.php
+++ config/FHIRConfig.php
@@ -4,5 +4,9 @@ return array(
 
     'StripeApiKey' => 'sk_test_j0q30epRT5VPWYa0UbC68z0l',
     'Currency' => 'EUR',
-
+    'oxygen8_host' => '',
+    'oxygen8_url' => '',
+    'oxygen8_port' => '',
+    'oxygen8_username' => '',
+    'oxygen8_password' => ''
 );
